269654b8db6113ba77209b09bfdad0258eb42f27
Johann Markl <johann.markl@gmail.com>
2013-03-31 16:02:40 +0200

    add arguments to eat- and remember-parser
    - eat-parser handling of -n argument not yet established
    - remember-parser might get information of whether calories are
      per portion or per 100g

--- diet.py
+++ diet.py
@@ -24,10 +24,34 @@ parser = argparse.ArgumentParser(
         It aims to offer the capabilty of remembering the total calorie
         consumption per day.''')
 subparsers = parser.add_subparsers(title='available commands')
+
 eat_parser = subparsers.add_parser('eat',
+    description='''This command either looks up a given food in the database
+        and adds it's calories to the daily total or adds the directly given
+        amount of calories.''',
+    usage='%(prog)s [-h] ([-c CAL] | FOOD)',
     help='add calories to the daily calorie count')
+
+# either enter number of calories or food name:
+choice_group = eat_parser.add_mutually_exclusive_group(required=True)
+choice_group.add_argument('food', nargs='?', metavar='FOOD',
+    help='the name of the food to add')
+choice_group.add_argument('-c', metavar='CAL',
+    help='the number of calories per portion to add, instead of FOOD')
+
+# needs to consider when calorie amount is given per 100g
+# entering weight in grams might be preferred.
+eat_parser.add_argument('-n', metavar='NUM',
+    help='the number of times a calorie amount is added')
+
 remember_parser = subparsers.add_parser('remember',
     help='remember the calories of an item of food')
+remember_parser.add_argument('food', metavar='FOOD',
+    help='the name of the food to remember')
+remember_parser.add_argument('calories', metavar='CAL',
+    help='the number of calories of that item of food')
+remember_parser.add_argument('description', metavar='DESC', nargs='?',
+    help='an optional description')
 
 if __name__ == '__main__':
     parser.parse_args()
