ca6e917905f7a34fea9e22c27dd61936db4a1f68
Johann Markl <johann.markl@gmail.com>
2013-03-31 14:13:06 +0200

    raw skeleton of the command line parser, first two subcommands

--- diet.py
+++ diet.py
@@ -3,3 +3,18 @@
 capability of remembering the total calorie consumption per day.
 '''
 
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='''diet is a minimalistic calorie tracking program.
+        It aims to offer the capabilty of remembering the total calorie
+        consumption per day.''')
+subparsers = parser.add_subparsers(title='available commands')
+eat_parser = subparsers.add_parser('eat',
+    help='add calories to the daily calorie count')
+remember_parser = subparsers.add_parser('remember',
+    help='remember the calories of an item of food')
+
+if __name__ == '__main__':
+    parser.parse_args()
+
